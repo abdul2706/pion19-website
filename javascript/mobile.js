94011571a5d61d4f460e8065debf3d7e63124408
Abdul Rehman Khan <abdulrehmankhan27061998@gmail.com>
2019-07-28 11:07:10 +0500

    responsive at mobile, desktop

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -13,22 +13,25 @@ $(function() {
         console.log(TAG + 'menu-icon click: ends');
     });
 
-    $('#section-image-slider #slide-left-button').click(function() {
-        $('#section-image-slider img').attr('src', images[(--imageIndex) % images.length]);
+    $('#section-image-slider .slide-left-button').click(function() {
+        imageIndex = (imageIndex - 1) % images.length;
+        imageIndex = (imageIndex < 0) ? imageIndex + images.length : imageIndex;
+        $('#section-image-slider img').attr('src', images[imageIndex]);
     });
 
-    $('#section-image-slider #slide-right-button').click(function() {
-        $('#section-image-slider img').attr('src', images[(++imageIndex) % images.length]);
+    $('#section-image-slider .slide-right-button').click(function() {
+        imageIndex = (imageIndex + 1) % images.length;
+        $('#section-image-slider img').attr('src', images[imageIndex]);
     });
 
-    $('#section-video-slider #slide-left-button').click(function() {
+    $('#section-video-slider .slide-left-button').click(function() {
         videoIndex = (videoIndex - 1) % videos.length;
         videoIndex = (videoIndex < 0) ? videoIndex + videos.length : videoIndex;
         $('#section-video-slider video source').attr('src', videos[videoIndex]);
         $('#section-video-slider video')[0].load();
     });
 
-    $('#section-video-slider #slide-right-button').click(function() {
+    $('#section-video-slider .slide-right-button').click(function() {
         videoIndex = (videoIndex + 1) % videos.length;
         $('#section-video-slider video source').attr('src', videos[videoIndex]);
         $('#section-video-slider video')[0].load();
